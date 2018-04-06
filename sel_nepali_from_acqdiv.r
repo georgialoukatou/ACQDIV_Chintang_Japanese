a531f61f6eb32ec59d0daf83dd1b94255c8548ed
georgialoukatou <georgialoukatou@gmail.com>
2018-04-06 09:57:45 +0200

    Update sel_nepali_from_acqdiv.r

--- sel_nepali_from_acqdiv.r
+++ sel_nepali_from_acqdiv.r
@@ -3,7 +3,7 @@ acq<-load("/Users/lscpuser/Documents/acqdiv_corpus_2017-09-28_CRJ.rda")  ##compl
 
 morpheme_data<-data.frame(words)
 clean_corpus<-morpheme_data[ , c('word_language', "word")]
-language_data<-clean_corpus[clean_corpus$word_language=="English", ] ## or Nepali, both have been removed
+language_data<-clean_corpus[clean_corpus$word_language=="English", ] ## or Nepali, both must be removed
 list.var<-unique(language_data$word)
 options(max.print=70000)
 
