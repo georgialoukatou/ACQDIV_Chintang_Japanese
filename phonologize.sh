2645d2fd291ec1d4ac8072a575b76952db0c7187
alecristia <alecristia@gmail.com>
2018-04-17 20:10:19 +0200

    selection and cleaning in R working, bug in syllabification (wrong format out)

--- phonologize.sh
+++ phonologize.sh
@@ -183,6 +183,10 @@ echo processing $ORTHO into $RESULT_FOLDER
 
 		echo "removing blank lines"
 		LANG=C LC_CTYPE=C LC_ALL=C
+		cat $RESULT_FOLDER/${COVERAGE}_outofperl.tmp | 
+		sed 's/\// ;esyll /g' | #replace syll boundaries with our syll tag
+		sed 's/ / ;word /g' | #replace word boundaries with our word tag
+
 		sed '/^$/d' $RESULT_FOLDER/${COVERAGE}_outofperl.tmp |
 		sed 's/^[ ]*//g'  | # delete sentence-initial blanks
 		sed 's/[ ]*$//g' | # delete sentence-final blanks
