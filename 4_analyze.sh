f4386950f1bfcc094c2f9850dfa8f6ed6551a9bd
alecristia <alecristia@gmail.com>
2018-05-08 12:44:31 +0200

    cleaning

--- 4_analyze.sh
+++ 4_analyze.sh
@@ -3,24 +3,6 @@
 # By Georgia Loukatou georgialoukatou@gmail.com
 # Last changed Alex Cristia alecristia@gmail.com 2018-04-13
 
-#- passing some parameters
-#$-S /bin/bash        # the shell used
-#$-N big_wrap  # gives the name of the job
-#$-pe openmpi_ib 1   # nb of cores required (this is purely declarative)
-#$-j yes   # join stdout and stderr in the same file
-#$-q cpu   # name of the queue (cpu, gpu, all)
-#$-cwd     # puts the logs in the current directory
-
-#- some messages to the user
-/bin/echo Running on host: `hostname`.
-/bin/echo In directory: `pwd`
-echo QUEUE=$QUEUE
-echo NSLOTS=$NSLOTS
-/bin/echo Starting on: `date`
-
-#- some back magic
-#. /etc/profile.d/modules.sh
-
 ######################## Get info from upstream
 #file/folder information
 SCRIPT_FOLDER=$1
@@ -55,36 +37,33 @@ echo $THISGOLD $out
 
 
 	cat $THISTAG | wordseg-prep -u syllable --gold $THISGOLD > $PREP_SYLL
-#	cat $THISTAG | wordseg-prep -u phone > $PREP_PHONE
+	cat $THISTAG | wordseg-prep -u phone > $PREP_PHONE
 
-#	cat $PREP_SYLL | wordseg-tp -t relative -p forward > ${THISTAG}_segmented.ftp_rel.txt
-#	cat ${THISTAG}_segmented.ftp_rel.txt | wordseg-eval $THISGOLD > ${THISTAG}_eval.ftp_rel.txt
+	cat $PREP_SYLL | wordseg-tp -t relative -p forward > ${THISTAG}_segmented.ftp_rel.txt
+	cat ${THISTAG}_segmented.ftp_rel.txt | wordseg-eval $THISGOLD > ${THISTAG}_eval.ftp_rel.txt
 
-#	cat $PREP_SYLL | wordseg-tp -t absolute -p forward > ${THISTAG}_segmented.ftp_abs.txt
-#	cat ${THISTAG}_segmented.ftp_abs.txt | wordseg-eval $THISGOLD > ${THISTAG}_eval.ftp_abs.txt
+	cat $PREP_SYLL | wordseg-tp -t absolute -p forward > ${THISTAG}_segmented.ftp_abs.txt
+	cat ${THISTAG}_segmented.ftp_abs.txt | wordseg-eval $THISGOLD > ${THISTAG}_eval.ftp_abs.txt
 
-#	cat $PREP_SYLL | wordseg-tp -t relative -p backward > ${THISTAG}_segmented.btp_rel.txt
-#	cat ${THISTAG}_segmented.btp_rel.txt | wordseg-eval $THISGOLD > ${THISTAG}_eval.btp_rel.txt
+	cat $PREP_SYLL | wordseg-tp -t relative -p backward > ${THISTAG}_segmented.btp_rel.txt
+	cat ${THISTAG}_segmented.btp_rel.txt | wordseg-eval $THISGOLD > ${THISTAG}_eval.btp_rel.txt
 
-#	cat $PREP_SYLL | wordseg-tp -t absolute -p backward > ${THISTAG}_segmented.btp_abs.txt
-#	cat ${THISTAG}_segmented.btp_abs.txt | wordseg-eval $THISGOLD > ${THISTAG}_eval.btp_abs.txt
+	cat $PREP_SYLL | wordseg-tp -t absolute -p backward > ${THISTAG}_segmented.btp_abs.txt
+	cat ${THISTAG}_segmented.btp_abs.txt | wordseg-eval $THISGOLD > ${THISTAG}_eval.btp_abs.txt
 
-#	wordseg-dibs -t phrasal -o ${THISTAG}_segmented.dibs.txt $PREP_PHONE  $THISTAG
-#	wordseg-eval -o ${THISTAG}_eval.dibs.txt ${THISTAG}_segmented.dibs.txt $THISGOLD
+	wordseg-dibs -t phrasal -o ${THISTAG}_segmented.dibs.txt $PREP_PHONE  $THISTAG
+	wordseg-eval -o ${THISTAG}_eval.dibs.txt ${THISTAG}_segmented.dibs.txt $THISGOLD
 
-#        echo "module load python-anaconda boost && \
-#          source activate /cm/shared/apps/python-anaconda/envs/wordseg && \
-#          cat $PREP_PHONE | wordseg-ag $SCRIPT_FOLDER/Colloc0_acqdiv.lt Colloc0 --njobs 8 | tee ${THISTAG}_segmented.ag.txt | wordseg-eval $THISGOLD  > ${THISTAG}_eval.ag.txt || exit 1"   | qsub -S /bin/bash -V -cwd -j y -pe mpich 8 -N acqdiv_$FOLDER 
+        echo "module load python-anaconda boost && \
+          source activate /cm/shared/apps/python-anaconda/envs/wordseg && \
+          cat $PREP_PHONE | wordseg-ag $SCRIPT_FOLDER/Colloc0_acqdiv.lt Colloc0 --njobs 8 | tee ${THISTAG}_segmented.ag.txt | wordseg-eval $THISGOLD  > ${THISTAG}_eval.ag.txt || exit 1"   | qsub -S /bin/bash -V -cwd -j y -pe mpich 8 -N acqdiv_$FOLDER 
 
 	#baselines
-#	cat $PREP_SYLL | wordseg-baseline -P 1 > ${THISTAG}_segmented.baselinesyll1.txt
-#	cat ${THISTAG}_segmented.baselinesyll1.txt | wordseg-eval $THISGOLD > ${THISTAG}_eval.baselinesyll1.txt
+	cat $PREP_SYLL | wordseg-baseline -P 1 > ${THISTAG}_segmented.baselinesyll1.txt
+	cat ${THISTAG}_segmented.baselinesyll1.txt | wordseg-eval $THISGOLD > ${THISTAG}_eval.baselinesyll1.txt
 
-#	cat $PREP_SYLL | wordseg-baseline -P 0 > ${THISTAG}_segmented.baselinesyll0.txt
-#	cat ${THISTAG}_segmented.baselinesyll0.txt | wordseg-eval $THISGOLD > ${THISTAG}_eval.baselinesyll0.txt
-
-#	cat $PREP_SYLL | wordseg-baseline -P 0.5 > ${THISTAG}_segmented.baselinesyll0.5.txt
-#	cat ${THISTAG}_segmented.baselinesyll0.5.txt | wordseg-eval $THISGOLD > ${THISTAG}_eval.baselinesyll0.5.txt
+	cat $PREP_SYLL | wordseg-baseline -P 0 > ${THISTAG}_segmented.baselinesyll0.txt
+	cat ${THISTAG}_segmented.baselinesyll0.txt | wordseg-eval $THISGOLD > ${THISTAG}_eval.baselinesyll0.txt
 
 	echo "done segmentation"
 
--- BIG_WRAP.sh
+++ BIG_WRAP.sh
@@ -9,8 +9,7 @@
 #INPUT_FILE="/Users/acristia/Documents/acqdiv/acqdiv_corpus_2017-09-28_CRJ.rda" #where the database is
 SCRIPT_FOLDER="/scratch1/users/acristia/acqdiv/ACQDIV_Chintang_Japanese" #where the scripts are
 ROOT="/scratch1/users/acristia/acqdiv/" #where you want results to be saved
-SELECTION="full" # by default, this script analyzes the full database; you can also rerun it with the option "noforeign", in which case we recommend the N_PARTS to be 5
-N_PARTS=1 # number of subcorpora to use to be able to draw confidence intervals
+N_PARTS=10 # number of subcorpora to use to be able to draw confidence intervals
 ####################### USER, ALL DONE!
 
 #------------ corpus preparation stage ------------#
@@ -20,34 +19,38 @@ N_PARTS=1 # number of subcorpora to use to be able to draw confidence intervals
 #RScript $SCRIPT_FOLDER/1_sel_clean.r $INPUT_FILE $ROOT
 
 # phonologize ALL the files in the root folder
-# bash $SCRIPT_FOLDER/2_phonologize.sh $SCRIPT_FOLDER $ROOT $SELECTION
+# bash $SCRIPT_FOLDER/2_phonologize.sh $SCRIPT_FOLDER $ROOT full
+# bash $SCRIPT_FOLDER/2_phonologize.sh $SCRIPT_FOLDER $ROOT noforeign
 
 # cut the ensuing files into N_PARTS subparts
-if [ "$N_PARTS" -gt 1 ] ; then
-    for FILE in $ROOT/*/*/*-tags.txt ; do
-    	bash $SCRIPT_FOLDER/3_cut.sh $FILE $N_PARTS
-    done
-fi
+#if [ "$N_PARTS" -gt 1 ] ; then
+#    for FILE in $ROOT/*/*/*-tags.txt ; do
+#    	bash $SCRIPT_FOLDER/3_cut.sh $FILE $N_PARTS
+#    done
+#fi
 
 
 #------------ corpus analysis stage ------------#
 
 
-for LANGUAGE in Chintang  ; do
-    for LEVEL in  words ; do
+for LANGUAGE in Chintang Japanese ; do
+    for LEVEL in  words morphemes ; do
 echo $LANGUAGE/$LEVEL
 		#derive local vars
-		RESULT_FOLDER="$ROOT/$LANGUAGE/$LEVEL" 
-		bash  $SCRIPT_FOLDER/4_analyze.sh $SCRIPT_FOLDER $RESULT_FOLDER $N_PARTS $SELECTION 
-
-
-
+#		RESULT_FOLDER="$ROOT/$LANGUAGE/$LEVEL" 
+#		bash  $SCRIPT_FOLDER/4_analyze.sh $SCRIPT_FOLDER $RESULT_FOLDER $N_PARTS full 
+#		bash  $SCRIPT_FOLDER/4_analyze.sh $SCRIPT_FOLDER $RESULT_FOLDER 1 full 
+#		if [ "$LANGUAGE" = "Chintang" ] ; then
+#			bash  $SCRIPT_FOLDER/4_analyze.sh $SCRIPT_FOLDER $RESULT_FOLDER 1 noforeign 
+#		fi
     done
 done
 
-while [[ `qstat  | grep "acqdiv" | wc -l` -gt 0 ]] ; do
-    sleep 1
-done
+#------------ collapse results stage ------------#
+
+#while [[ `qstat  | grep "acqdiv" | wc -l` -gt 0 ]] ; do
+#    sleep 1
+#done
 
 #bash $SCRIPT_FOLDER/5_collapse_results.sh $ROOT
 
